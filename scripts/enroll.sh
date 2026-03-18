77bb25563c9f79d47330b73fd93c8e6a0ae907f6
vegcom <189027736+vegcom@users.noreply.github.com>
2026-03-18 16:27:43 -0500

    fix: enrollment default to env vars for linux

--- scripts/enroll.sh
+++ scripts/enroll.sh
@@ -3,4 +3,4 @@
 read -p "Salt Master Master : " salt_master
 read -p "Minion ID: " minion_id
 
-curl -L https://raw.githubusercontent.com/saltstack/salt-bootstrap/develop/bootstrap-salt.sh | sh -s -- -A "${salt_master}" -i "${minion_id}" onedir
+curl -L https://raw.githubusercontent.com/saltstack/salt-bootstrap/develop/bootstrap-salt.sh | sh -s -- -A "${salt_master:-$SALT_MASTER}" -i "${minion_id:-MINION_ID}" onedir
