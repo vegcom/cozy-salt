d36881e0b1c08e832b7b2a4545777e10f779b674
vegcom <189027736+vegcom@users.noreply.github.com>
2026-08-14 14:01:17 -0500

    fix: correct indentation in 3-miniforge.sh script

--- provisioning/linux/files/opt-cozy-etc-profile.d/3-miniforge.sh
+++ provisioning/linux/files/opt-cozy-etc-profile.d/3-miniforge.sh
@@ -8,12 +8,12 @@ export CONDA_PKGS_DIRS="${CONDA_PKGS_BASE}/${UID:-$(id -u)}"
 
 export CONDA_BASE_PATH="${CONDA_BASE_PATH:-/opt/miniforge3/bin/}"
 
-if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ] && [ -n "$SHELL" ] ; then
-  _SHELL_NAME="$(basename ${SHELL})"
-  eval "$($CONDA_BASE_PATH/conda shell.$_SHELL_NAME hook)" >/dev/null
+if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ] && [ -n "$SHELL" ]; then
+	_SHELL_NAME="$(basename ${SHELL})"
+	eval "$($CONDA_BASE_PATH/conda shell.$_SHELL_NAME hook)" >/dev/null
 	eval "$($CONDA_BASE_PATH/mamba shell hook --shell ${_SHELL_NAME})" >/dev/null
 fi
 
-if [ -f "${CONDA_BASE_PATH}/conda" ];then
-  ${CONDA_BASE_PATH}/conda config --remove envs_dirs "/home/${USER}/.conda/envs" >/dev/null 2>&1 || true
+if [ -f "${CONDA_BASE_PATH}/conda" ]; then
+	${CONDA_BASE_PATH}/conda config --remove envs_dirs "/home/${USER}/.conda/envs" >/dev/null 2>&1 || true
 fi
