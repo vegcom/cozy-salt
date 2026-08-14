f11ec60ae4e209a1e04be19a3389030009d3b253
vegcom <189027736+vegcom@users.noreply.github.com>
2026-08-14 16:13:36 -0500

    fix: move CONDA_EXE and MAMBA_EXE exports iin opt-cozy-etc profile and environment

--- provisioning/linux/files/opt-cozy-etc-environment.d/3-miniforge.sh
+++ provisioning/linux/files/opt-cozy-etc-environment.d/3-miniforge.sh
@@ -2,10 +2,7 @@
 # Managed by Salt - DO NOT EDIT MANUALLY
 
 # Miniforge conda/mamba initialization (system-wide installation at /opt/miniforge3)
-
 export CONDA_AUTO_ACTIVATE_BASE=false
 export CONDA_CHANGEPS1=false
 export CONDA_ENVS_DIRS='/opt/miniforge3/envs'
-export CONDA_EXE="$CONDA_BASE_PATH/conda"
 export CONDA_ROOT_PREFIX='/opt/miniforge3'
-export MAMBA_EXE="$CONDA_BASE_PATH/mamba"
